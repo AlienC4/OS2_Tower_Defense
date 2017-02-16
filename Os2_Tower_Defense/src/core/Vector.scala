c0b40d4af5d7df0a3938448cf2fde6bfcfc15acf
Julius <outsi@LAPTOP-B75H6AO3>
2017-02-16 16:28:59 +0200

    Added a class representing mathematical vectors. Added basic functions in the class.

--- Os2_Tower_Defense/src/core/Vector.scala
+++ Os2_Tower_Defense/src/core/Vector.scala
@@ -1,5 +1,19 @@
 package core
 
-class Vector(x: Double, y: Double) {
+import scala.math._
+
+case class Vector(x: Double, y: Double) {
   
+  def r = hypot(x, y)
+  def theta = atan2(y, x)
+  
+  def +(v: Vector) = Vector(x + v.x, y + v.y)
+  
+  implicit class mult(m: Double) {
+     def ^(a: Double) = pow(m, a)
+     def *(v: Vector) = Vector(m * x, m * y)
+  }
 }
+
+
+
