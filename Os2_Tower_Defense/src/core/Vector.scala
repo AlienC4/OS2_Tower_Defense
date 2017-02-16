49961994d53eca80692b991d882547d7ddf90167
outsi <outsi@LAPTOP-B75H6AO3>
2017-02-16 16:53:36 +0200

    Added * and unit methods.

--- Os2_Tower_Defense/src/core/Vector.scala
+++ Os2_Tower_Defense/src/core/Vector.scala
@@ -8,10 +8,14 @@ case class Vector(x: Double, y: Double) {
   def theta = atan2(y, x)
   
   def +(v: Vector) = Vector(x + v.x, y + v.y)
+  def *(m: Double) = m * this
+  
+  def unit = (1/r) * this
+  
   
   implicit class mult(m: Double) {
      def ^(a: Double) = pow(m, a)
-     def *(v: Vector) = Vector(m * x, m * y)
+     def *(v: Vector): Vector = Vector(m * x, m * y)
   }
 }
 
