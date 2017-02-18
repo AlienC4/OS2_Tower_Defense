b6e897e7210d9cc2e28220dd11ceb41cfad34384
outsi <outsi@LAPTOP-B75H6AO3>
2017-02-19 00:51:00 +0200

    Documented the methods.

--- Os2_Tower_Defense/src/core/Vector2D.scala
+++ Os2_Tower_Defense/src/core/Vector2D.scala
@@ -4,13 +4,20 @@ import scala.math._
 
 case class Vector2D(x: Double, y: Double) {
   
+  /** Gives the length of this vector */
   def r = hypot(x, y)
+  /** Gives the signed angle between this vector and the x-axis */
   def theta = atan2(y, x)
   
+  /** Adds two Vectors together */
   def +(v: Vector2D) = Vector2D(x + v.x, y + v.y)
+  /** Multiplies the components of this vector by a given value, effectively strecting the vector */
   def *(m: Double) = m * this
+  /** Divides the components of this vector by a given value, effectively shrinking the vector */
+  def /(m: Double) = Vector2D(x / m, y / m)
   
-  def unit = (1/r) * this
+  /** Gives a vector that has the same heading as this but has a length of 1 */
+  def unit = this / r
   
   
   implicit class mult(m: Double) {
