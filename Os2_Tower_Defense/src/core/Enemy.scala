f023a4ed2e3b1d835a36adde69c1e086d439cfa6
outsi <outsi@LAPTOP-B75H6AO3>
2017-02-19 00:54:26 +0200

    Added some basic methods. Preparations for path following.

--- Os2_Tower_Defense/src/core/Enemy.scala
+++ Os2_Tower_Defense/src/core/Enemy.scala
@@ -1,5 +1,17 @@
 package core
 
-case class Enemy(health: Double, speed: Double) {
+case class Enemy(var health: Double, var speed: Vector2D, var pos: Vector2D) {
   
+  var path: Path = null
+  var currentNode: Int = 0
+  
+  def alive = this.health > 0
+  def dead = !alive
+  
+  def hit(damage: Double) = health -= damage
+  
+}
+
+trait Flying {
+  val flying = true
 }
