0477dc136b832c66efa881197d3558a37f1e59b1
outsi <outsi@LAPTOP-B75H6AO3>
2017-03-13 15:03:30 +0200

    Added methods for handling the enemies in the waves.

--- Os2_Tower_Defense/src/core/Wave.scala
+++ Os2_Tower_Defense/src/core/Wave.scala
@@ -1,5 +1,10 @@
 package core
 
-case class Wave() {
+case class Wave(private var enemies: Vector[Enemy]) {
+  def addEnemy(enemy: Enemy) = enemies ++= Vector(enemy)
+  def addEnemy(enemy: Enemy*) = enemies ++= enemy.toVector
   
+  def setPaths(path: Path) = enemies.map(_.setPath(path))
+  
+  def isDone = enemies.forall(_.dead)
 }
