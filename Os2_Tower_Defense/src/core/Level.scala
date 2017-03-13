564a9fdaade5c33996d4145469aeb3aa848296f8
outsi <outsi@LAPTOP-B75H6AO3>
2017-03-13 14:59:57 +0200

    Added the possibility to add multiple waves at once. Added an apply-method.

--- Os2_Tower_Defense/src/core/Level.scala
+++ Os2_Tower_Defense/src/core/Level.scala
@@ -5,8 +5,9 @@ case class Level(path: Path) {
   private var waves: Vector[Wave] = Vector()
   
   def addWave(wave: Wave) = this.waves ++= Vector(wave)
+  def addWave(wave: Wave*) = this.waves ++= wave.toVector
   
   def getWave(n: Int) = waves(n)
-  
+  def apply(n: Int) = getWave(n)
   
 }
