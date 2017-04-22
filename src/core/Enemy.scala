1e2e67d5d95ccafefd5e745c9f24128a375e2ec6
AlienC4 <outsider.2009@hotmail.com>
2017-04-22 19:20:42 +0300

    Added maxSpeed for enemies

--- src/core/Enemy.scala
+++ src/core/Enemy.scala
@@ -1,10 +1,12 @@
 package core
 
-case class Enemy(var health: Double, var speed: Vector2D, var pos: Vector2D) {
+case class Enemy(var health: Double, var speed: Vector2D, var pos: Vector2D = Vector2D(0, 0)) {
   
   private var path: Path = null
   private var currentNode: Int = 0
   private val distance = 10
+  private var maxSpeed: Double = 10
+  
   def cn = currentNode
   
   def alive = this.health > 0
@@ -18,6 +20,7 @@ case class Enemy(var health: Double, var speed: Vector2D, var pos: Vector2D) {
   
   def getPath = this.path
   def setPath(p: Path) = this.path = p
+  def setMaxSpeed(s: Int) = maxSpeed = s
   
   /** Returns a vector pointing to the given target */
   def desiredVelocity(target: Vector2D) = (target - this.pos).unit * this.speed.r
@@ -30,8 +33,9 @@ case class Enemy(var health: Double, var speed: Vector2D, var pos: Vector2D) {
   def move = {
     val target = this.path(cn)
     this.speed = this.steering(target) + this.speed
+    this.speed = trunc(this.speed, maxSpeed)
     this.pos += this.speed
-    if ((this.pos.r - target.r) <= this.distance && this.cn < this.path.length) {
+    if ((this.pos - target).r <= this.distance && this.cn < this.path.length) {
       this.currentNode += 1
     }
   }
