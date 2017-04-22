1d8cb676ed4ca32781d07af8cc5d62c7c460efc8
AlienC4 <outsider.2009@hotmail.com>
2017-04-23 02:43:46 +0300

    Set distance to 3 from 10

--- src/core/Enemy.scala
+++ src/core/Enemy.scala
@@ -4,7 +4,7 @@ case class Enemy(var health: Double, var speed: Vector2D, var pos: Vector2D = Ve
   
   private var path: Path = null
   private var currentNode: Int = 0
-  private val distance = 10
+  private val distance = 3
   private var maxSpeed: Double = 10
   
   def cn = currentNode
@@ -32,7 +32,7 @@ case class Enemy(var health: Double, var speed: Vector2D, var pos: Vector2D = Ve
   /** Moves the enemy towards the targeted node */
   def move = {
     val target = this.path(cn)
-    this.speed = this.steering(target) + this.speed
+    this.speed += this.steering(target)
     this.speed = trunc(this.speed, maxSpeed)
     this.pos += this.speed
     if ((this.pos - target).r <= this.distance && this.cn < this.path.length) {
