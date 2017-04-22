3e466ee03a5b0898f187a7f4d999a6119363ef48
AlienC4 <outsider.2009@hotmail.com>
2017-04-23 02:44:33 +0300

    Added currentWave & co.

--- src/core/Level.scala
+++ src/core/Level.scala
@@ -3,9 +3,14 @@ package core
 
 case class Level(path: Path) {
   private var waves: Vector[Wave] = Vector()
+  private var c = 0
   
   def addWave(wave: Wave) = this.waves ++= Vector(wave)
   def addWave(wave: Wave*) = this.waves ++= wave
+  
+  def hasNext = c >= 0 && c < waves.size
+  def currentWave = getWave(c)
+  def nextWave = c += 1
   
   def getWave(n: Int) = waves(n)
   def apply(n: Int) = getWave(n)
