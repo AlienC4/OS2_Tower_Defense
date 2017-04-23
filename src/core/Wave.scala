6ee699cb315b22b0630d152770a5b08062f0da1d
AlienC4 <outsider.2009@hotmail.com>
2017-04-23 20:56:54 +0300

    Added getEnemy, apply, size and length methods

--- src/core/Wave.scala
+++ src/core/Wave.scala
@@ -5,9 +5,15 @@ case class Wave(private var enemies: Vector[Enemy]) {
   def addEnemy(enemy: Enemy*) = enemies ++= enemy
   
   def getEnemies = this.enemies
+  def getEnemy(n: Int) = this.enemies(n)
+  
   
   def setPaths(path: Path) = enemies.map(_.setPath(path))
-  def setPositions(pos: Vector2D) = enemies.map(_.pos = pos) 
+  def setPositions(pos: Vector2D) = enemies.foreach(e => e.setPos(pos)) 
   
   def isDone = enemies.forall(e => e.dead || e.isAtEndOfPath)
+  
+  def apply(n: Int) = this.enemies(n)
+  def size = this.enemies.size
+  def length = this.enemies.length
 }
