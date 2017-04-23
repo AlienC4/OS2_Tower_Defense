687f4b9edf25ac00e1a9c5ba31fc6e9a4bf4c3c9
AlienC4 <outsider.2009@hotmail.com>
2017-04-23 15:43:05 +0300

    Added getEnemies and modified isDone

--- src/core/Wave.scala
+++ src/core/Wave.scala
@@ -4,8 +4,10 @@ case class Wave(private var enemies: Vector[Enemy]) {
   def addEnemy(enemy: Enemy) = enemies ++= Vector(enemy)
   def addEnemy(enemy: Enemy*) = enemies ++= enemy
   
+  def getEnemies = this.enemies
+  
   def setPaths(path: Path) = enemies.map(_.setPath(path))
   def setPositions(pos: Vector2D) = enemies.map(_.pos = pos) 
   
-  def isDone = enemies.forall(_.dead)
+  def isDone = enemies.forall(e => e.dead || e.isAtEndOfPath)
 }
