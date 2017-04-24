a015ca9a64f2aaded933cac2f9a25f346d87091a
AlienC4 <outsider.2009@hotmail.com>
2017-04-24 04:35:51 +0300

    Drawing and placing towers

--- src/GUI/Main.scala
+++ src/GUI/Main.scala
@@ -4,7 +4,7 @@ import core._
 import processing.core._
 import processing.core.PConstants._
 import scala.math.Pi
-import java.awt.Dimension
+import scala.collection.mutable.Buffer
 
 object Main extends PApplet {
 
@@ -14,19 +14,25 @@ object Main extends PApplet {
   val level1 = loadLevel(1)
   val enemy1 = loadEnemy("type1")
   val pic1 = loadImage("enemies/Enemy1.png")
+  val tower1 = loadTower("basic")
+  val base = loadImage("towers/Tower.png")
+  val turret = loadImage("towers/Turret.png")
   pic1.resize(32, 0)
+  val towerPos = Buffer[(Int, Int)]()
+  val towers = Buffer[Tower]()
+  var showRanges = false
 
   def cw = level1.currentWave
+  
   for (i <- 0 until cw.size) {
     cw(i).setPos(cw(i).getPos.offset(0, 10 * (i + 1)))
   }
-//  level1.getWave(0).getEnemies.foreach(e => println(e.getPos))
 
   val side = 32
   val off = 0.5f
 
   def drawGrid(hei: Int, wid: Int, size: Int, offset: Float) {
-    stroke(0x000000)
+    stroke(0, 0, 0)
     for (i <- 1 to hei / size) {
       line(0, (i - offset) * size, w, (i - offset) * size)
     }
@@ -47,15 +53,18 @@ object Main extends PApplet {
       rot(e) 
       drawHealth(e)
     }
-    
+    towers.foreach { t =>
+      rot(t)
+    }
     if (millis() >= 5000) {
       cw.getEnemies.foreach(e => e.move)
+      towers.foreach(t => t.shoot)
     }
       
   }
 
   /** Rotates the given enemy around it's center point */
-  private def rot(e: Enemy) = {
+  private def rot(e: Enemy): Unit = {
     translate(e.x, e.y)
     rotate((e.speed.theta + Pi).toFloat)
     rotate(Pi.toFloat)
@@ -64,8 +73,22 @@ object Main extends PApplet {
     image(pic1, e.x, e.y)
     imageMode(CORNER)
     translate(e.x, e.y)
-    rotate(-(e.speed.theta + 2*Pi).toFloat)
+    rotate(-(e.speed.theta + 2 * Pi).toFloat)
     translate(-e.x, -e.y)
+  }
+  
+  private def rot(t: Tower): Unit = {
+    imageMode(CENTER)
+    image(base, t.x, t.y)
+    translate(t.x, t.y)
+    rotate(t.theta)
+    rotate((Pi/2).toFloat)
+    translate(-t.x, -t.y)
+    image(turret, t.x, t.y)
+    imageMode(CORNER)
+    translate(t.x, t.y)
+    rotate(-(t.theta + (Pi/2).toFloat))
+    translate(-t.x, -t.y)
   }
   
   private def drawHealth(e: Enemy) = {
@@ -83,8 +106,22 @@ object Main extends PApplet {
   override def mouseClicked(): Unit = {
     val mx = (mouseX / side + off) * side
     val my = (mouseY / side + off) * side
-        Console.print(s"$mx, $my; ")
+    val pos = Vector2D(mx, my)
+    val p = (mx.toInt, my.toInt)
+    if (!towerPos.contains(p)) {
+      towerPos += p
+      towers += placeTower(pos, "basic")
+    }
+//    Console.print(s"$mx, $my; ")
 //    Console.print(s"$mouseX, $mouseY; ")
+  }
+  
+  
+  def placeTower(pos: Vector2D, tower: String) = {
+    val (range, damage, rof) = loadTower(tower)
+    val t = new Tower(pos, range, damage, rof)
+    t.level = level1
+    t
   }
 
   def main(args: Array[String]) {
@@ -102,5 +139,4 @@ object Main extends PApplet {
 
 }
 
-case class HealthBar(value: Double, max: Double, x: Float, y: Float, w: Float, h: Float) {
-}
+case class HealthBar(value: Double, max: Double, x: Float, y: Float, w: Float, h: Float)
