73810c9066dd363f470002b6ff5df7a639aab02c
AlienC4 <outsider.2009@hotmail.com>
2017-04-26 04:51:20 +0300

    Added costs to towers and balancing

--- src/core/Tower.scala
+++ src/core/Tower.scala
@@ -3,11 +3,12 @@ package core
 
 /**
  * @params pos: The position of the tower
+ * @param cost: The cost to place or upgrade the tower
  * @param range: The attack range of the tower
  * @param damage: the attack damage of the tower
  * @param rof: The rate of fire of the tower, in shots per second
  */
-case class Tower(pos: Vector2D, var range: Double, var damage: Double, var rof: Double) {
+case class Tower(pos: Vector2D, var cost: Int, var range: Double, var damage: Double, var rof: Double) {
   
   private var lastShot = System.currentTimeMillis() // Initially the time when the tower was created
   var lastTarget: Vector2D = Vector2D(0, 0)
@@ -19,14 +20,15 @@ case class Tower(pos: Vector2D, var range: Double, var damage: Double, var rof: 
   
   def upgrade = {
     this.upLevel += 1
-    this.range *= 1.2
-    this.damage *= 1.1
-    this.rof *= 1.05
+    this.range *= 1.1
+    this.damage *= 1.3
+    this.rof += 0.05
+    this.cost = (1.4 * this.cost).toInt
   }
   
   def shoot = {
     val shot = System.currentTimeMillis()
-    if (target.isDefined && target.get.alive && (shot - lastShot) > tbs) {
+    if (target.isDefined && target.get.alive && target.get.cn > 0 && (shot - lastShot) > tbs) {
       lastShot = shot
       lastTarget = target.get.pos
       target.get.hit(damage)
@@ -39,7 +41,7 @@ case class Tower(pos: Vector2D, var range: Double, var damage: Double, var rof: 
   
   def inRange: Vector[Enemy] = {
     def dist(e: Enemy) = level.path.lengthUntilNode(e.cn - 1) + (level.path(e.cn - 1) - e.pos).r
-    level.currentWave.getEnemies.filter(e => isInRange(e) && e.alive).sortBy(e => -dist(e))
+    level.currentWave.getEnemies.filter(e => isInRange(e) && e.alive && !e.isAtEndOfPath).sortBy(e => -dist(e))
   }
   
   def isInRange(e: Enemy): Boolean = (e.pos - this.pos).r <= this.range
