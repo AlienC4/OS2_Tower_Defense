e4adf6a530b2711386ae4b8031caaf17dbc08106
AlienC4 <outsider.2009@hotmail.com>
2017-04-26 04:56:12 +0300

    Slowing down at corners, added money and lives worth to enemies

--- src/core/Enemy.scala
+++ src/core/Enemy.scala
@@ -1,11 +1,11 @@
 package core
 
-case class Enemy(initHealth: Double, var speed: Vector2D, var pos: Vector2D, etype: String) {
+case class Enemy(initHealth: Double, var speed: Vector2D, var pos: Vector2D, moneyWorth: Int, livesWorth: Int, etype: String) {
 
-  private[this] var path: Path = null
-  private[this] var currentNode: Int = 0
+  private var path: Path = null
+  private var currentNode: Int = 0
   private val distance = 8
-  private[this] var maxSpeed: Double = 1.0
+  private var maxSpeed: Double = 1.0
 
   var health = initHealth
 
@@ -39,10 +39,11 @@ case class Enemy(initHealth: Double, var speed: Vector2D, var pos: Vector2D, ety
     if (!isAtEndOfPath && this.alive) {
       val target = this.path(cn)
       this.speed += this.steering(target)
-      this.speed *= 1.05
+      if (target.isWithin(32, this.pos) && this.speed.r * 1.05 >= maxSpeed) this.speed *= 0.8 else this.speed *= 1.05
+      
       this.speed = trunc(this.speed, maxSpeed)
       this.pos += this.speed
-      if ((this.pos - target).r <= this.distance && this.cn < this.path.length) {
+      if (target.isWithin(distance, this.pos) && this.cn < this.path.length) {
         this.currentNode += 1
       }
     }
