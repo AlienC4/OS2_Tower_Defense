64f39d75d52acc7b1fd3913c45961a37f332767d
AlienC4 <outsider.2009@hotmail.com>
2017-04-26 04:57:41 +0300

    Added reload method

--- src/core/Level.scala
+++ src/core/Level.scala
@@ -1,14 +1,14 @@
 package core
 
 
-case class Level(path: Path) {
+case class Level(number: Int, money: Int, lives: Int, path: Path) {
   private var waves: Vector[Wave] = Vector()
   private var c = 0
   
   def addWave(wave: Wave) = this.waves ++= Vector(wave)
   def addWave(wave: Wave*) = this.waves ++= wave
   
-  def hasNext = c >= 0 && c < waves.size
+  def hasNext = c >= 0 && c < waves.size - 1
   def currentWave = getWave(c)
   def nextWave = c += 1
   
@@ -16,5 +16,11 @@ case class Level(path: Path) {
   
   def getWave(n: Int) = waves(n)
   def apply(n: Int) = getWave(n)
+  
+  def reload() = {
+    val l = loadLevel(number)
+    waves = l.waves
+    c = 0
+  }
   
 }
