2654267ceb72bb7013429a6d1307b8284123fd72
AlienC4 <outsider.2009@hotmail.com>
2017-04-29 22:53:09 +0300

    Deleted length, angle and normalize methods since calling them would've required referring to a vector anyway. Added comments to previously uncommented methods

--- src/core/Vector2D.scala
+++ src/core/Vector2D.scala
@@ -6,32 +6,32 @@ case class Vector2D(x: Double, y: Double) {
   
   /** Gives the length of this vector */
   def r = hypot(x, y)
-  def length(v: Vector2D) = v.r
   /** Gives the signed angle between this vector and the x-axis in radians*/
   def theta = atan2(y, x)
-  def angle(v: Vector2D) = v.theta
   
   /** Adds two Vectors together */
   def +(v: Vector2D) = Vector2D(x + v.x, y + v.y)
+  /** Adds a tuple to this vector */
   def +(xy : (Double, Double)) = Vector2D(x + xy._1, y + xy._2)
   /** Substacts the given Vector from this */
   def -(v: Vector2D) = Vector2D(x - v.x, y - v.y)
-  /** Multiplies the components of this vector by a given value, effectively strecting the vector */
+  /** Multiplies the components of this vector by a given value, effectively stretching the vector */
   def *(m: Double) = m * this
   /** Divides the components of this vector by a given value, effectively shrinking the vector */
   def /(m: Double) = Vector2D(x / m, y / m)
   
   /** Gives a vector that has the same heading as this but has a length of 1 */
   def unit = this / r
-  def normalize(v: Vector2D) = v.unit
   
+  /** Returns a vector that is this vector offset by the value specified by ox and oy */
   def offset(ox: Double, oy: Double) = this + Vector2D(ox, oy)
   
+  /** Checks if the difference between this and that is smaller than radius */
   def isWithin(radius: Double, that: Vector2D) = (this - that).r <= radius
   
   
   implicit class mult(m: Double) {
-     def ^(a: Double) = pow(m, a)
+    /** Allows for writing vector-scalar multiplication both ways: m * v and v * m, where v is a vector and m is a number */
      def *(v: Vector2D): Vector2D = Vector2D(m * v.x, m * v.y)
   }
 }
